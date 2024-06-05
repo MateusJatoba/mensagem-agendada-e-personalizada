a91ad82c4a025487bffe4f2c791e701ca81792cc
Mateus Jatobá <125160451+MateusJatoba@users.noreply.github.com>
2024-06-05 15:45:22 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+import datetime
+
+def aplicacao():
+    from time import sleep
+
+    import pyautogui
+    import webbrowser
+    from urllib.parse import quote
+
+    TELEFONE = 999451205
+    mensagem = 'Pagar o INSS de Val'
+    MENSAGEM = quote(mensagem)
+    url = f'https://web.whatsapp.com/send?phone=55{TELEFONE}&text={MENSAGEM}'
+
+    webbrowser.open(url=url)
+    sleep(8)
+    pyautogui.press('enter')
+    sleep(5)
+    pyautogui.hotkey('ctrl' , 'w')
+    sleep(3)
+    pyautogui.hotkey('ctrl' , 'w')
+
+
+def checar_dia():
+    dia = datetime.datetime.today()
+
+    if dia == 5:
+        aplicacao()
+
+
+if __name__ == '__main__':
+    checar_dia()
+
